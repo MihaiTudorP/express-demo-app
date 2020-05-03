b1b96393c93bad42a7c26aa50530b863326e8de7
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-05-03 16:13:54 +0300

    Finished first draft version

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import {Router, Request, Response} from 'express';
+import {Router, Request, Response, NextFunction} from 'express';
 
 interface RequestWithBody extends Request{
     body: {[key: string]: string|undefined};
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction){
+    if(req.session && req.session.loggedIn){
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not Allowed');
 }
 
 const router = Router();
@@ -27,6 +37,7 @@ router.get('/', (req:Request, res: Response) => {
         res.send(`
             <div>
                 <div>You are logged in</div>
+                <div><a href="/protected">Protected route</a></div>
                 <a href="/logout">Logout</a>
             </div>
         `);
@@ -44,6 +55,10 @@ router.get('/logout', (req: Request, res: Response)=>{
     req.session = undefined;
     res.redirect('/');
 });
+
+router.get('/protected', requireAuth, (req:Request, res: Response) => {
+    res.send('Welcome to the protected route, logged in user');
+})
 
 router.post('/login', (req: RequestWithBody, res:Response) => {
     const{email, password} = req.body;
